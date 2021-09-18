6a72c39948b4db41266cc63f3935ea89bae9754e
hackycy <qa894178522@qq.com>
2021-09-18 14:15:21 +0800

    feat: 增加网络诊断弹窗

--- src/utils/index.js
+++ src/utils/index.js
@@ -139,7 +139,7 @@ export function toLine(name) {
 }
 
 export function getFileExtension(filename) {
-  return (/[.]/.exec(filename)) ? /[^.]+$/.exec(filename)[0] : undefined
+  return /[.]/.exec(filename) ? /[^.]+$/.exec(filename)[0] : undefined
 }
 
 /**
@@ -175,4 +175,50 @@ export function printANSI() {
  #    # #            #    # #    # #    # # #   ##
   ####  #            #    # #####  #    # # #    #
 `)
+}
+
+/**
+ * 判断操作系统
+ */
+export function detectOS() {
+  const sUserAgent = navigator.userAgent
+  const isWin =
+    navigator.platform === 'Win32' || navigator.platform === 'Windows'
+  const isMac =
+    navigator.platform === 'Mac68K' ||
+    navigator.platform === 'MacPPC' ||
+    navigator.platform === 'Macintosh' ||
+    navigator.platform === 'MacIntel'
+  if (isMac) return 'Mac'
+  var isUnix = navigator.platform === 'X11' && !isWin && !isMac
+  if (isUnix) return 'Unix'
+  var isLinux = String(navigator.platform).indexOf('Linux') > -1
+  if (isLinux) return 'Linux'
+  if (isWin) {
+    var isWin2K =
+      sUserAgent.indexOf('Windows NT 5.0') > -1 ||
+      sUserAgent.indexOf('Windows 2000') > -1
+    if (isWin2K) return 'Windows 2000'
+    var isWinXP =
+      sUserAgent.indexOf('Windows NT 5.1') > -1 ||
+      sUserAgent.indexOf('Windows XP') > -1
+    if (isWinXP) return 'Windows XP'
+    var isWin2003 =
+      sUserAgent.indexOf('Windows NT 5.2') > -1 ||
+      sUserAgent.indexOf('Windows 2003') > -1
+    if (isWin2003) return 'Windows 2003'
+    var isWinVista =
+      sUserAgent.indexOf('Windows NT 6.0') > -1 ||
+      sUserAgent.indexOf('Windows Vista') > -1
+    if (isWinVista) return 'Windows Vista'
+    var isWin7 =
+      sUserAgent.indexOf('Windows NT 6.1') > -1 ||
+      sUserAgent.indexOf('Windows 7') > -1
+    if (isWin7) return 'Windows 7'
+    var isWin8 = sUserAgent.indexOf('Windows NT 8.1') > -1
+    if (isWin8) return 'Windows 8'
+    var isWin10 = sUserAgent.indexOf('Windows NT 10.0') > -1
+    if (isWin10) return 'Windows 10'
+  }
+  return 'Unknown'
 }
