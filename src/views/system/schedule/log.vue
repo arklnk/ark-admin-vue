8025ee0b602492a2af37584a495b2fec5bc2ef67
hackycy <qa894178522@qq.com>
2021-10-08 10:46:20 +0800

    feat: 移除定时任务日志耗时字段

--- src/views/system/schedule/log.vue
+++ src/views/system/schedule/log.vue
@@ -6,11 +6,6 @@
         <el-table-column prop="taskId" label="任务编号" align="center" width="100" />
         <el-table-column prop="name" show-overflow-tooltip label="任务名称" align="center" width="200" />
         <el-table-column prop="detail" show-overflow-tooltip label="异常信息" align="center" />
-        <el-table-column prop="consumeTime" label="耗时" align="center" width="120">
-          <template slot-scope="scope">
-            <el-tag size="small" type="primary">{{ scope.row.consumeTime + ' ms' }}</el-tag>
-          </template>
-        </el-table-column>
         <el-table-column prop="status" label="状态" width="100" align="center">
           <template slot-scope="scope">
             <el-tag size="small" :type="getStatusType(scope.row.status)" effect="dark">{{
